150e12108f0c42c75ab145eb03c810cc2544736b
ZaicevDmitriy <zaicevdmitiy@gmail.com>
2017-11-28 00:26:13 +0300

    problem 5.3

---
+++ 5дз/5.3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include "polinomial.h"
+
+using namespace std;
+
+
+int main()
+{
+    cout << "enter degree your polinomial: \n";
+    int degree = 0;
+    cin >> degree;
+    int *coefficients = new int[degree + 1];
+    cout << "enter coefficients your polinomial: \n";
+    for (int i = 0; i < degree + 1; i++)
+        cin >> coefficients[i];
+    cout << "your polinomial: \n";
+    printDegrees(degree, coefficients);
+    cout << "\n";
+    printCoefficients(degree, coefficients);
+
+    delete [] coefficients;
+}
