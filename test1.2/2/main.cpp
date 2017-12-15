363c36c7e4d9a1435bad0026d3bb7f4b3a8c0f7d
ZaicevDmitriy <zaicevdmitiy@gmail.com>
2017-12-15 16:49:14 +0300

    created function, fixed if else

--- test1.2/2/main.cpp
+++ test1.2/2/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int biggest(int size1, int size2, bool* number1, bool* number2)
+int biggest(int size1, int size2, bool* firstNumber, bool* secondNumber)
 {
     if (size1 > size2)
         return 1;
@@ -10,18 +10,23 @@ int biggest(int size1, int size2, bool* number1, bool* number2)
         return 2;
     for (int i = size1 - 1; i >= 0; i--)
     {
-        if (number1[i])
-        {
-            if (!number2[i])
-                return 1;
-        }
-        else
-        {
-            if (number2[i])
-                return 2;
-        }
+        if (firstNumber[i] && !secondNumber[i])
+            return 1;
+        else if (secondNumber[i] && !firstNumber)
+            return 2;
     }
     return 0;
+}
+
+void printBiggest(int size1, int size2, bool* firstNumber, bool* secondNumber)
+{
+    int result = biggest(size1, size2, firstNumber, secondNumber);
+    if (result == 1)
+        cout << "first number > second number";
+    if (result == 2)
+        cout << "first number < second number";
+    if (result == 0)
+        cout << "first number = second number";
 }
 
 int main()
@@ -38,11 +43,5 @@ int main()
     cout << "enter second number: \n";
     for (int i = 0; i < size2; i++)
         cin >> secondNumber[i];
-    int result = biggest(size1, size2, firstNumber, secondNumber);
-    if (result == 1)
-        cout << "first number > second number";
-    if (result == 2)
-        cout << "first number < second number";
-    if (result == 0)
-        cout << "first number = second number";
+    printBiggest(size1, size2, firstNumber, secondNumber);
 }
