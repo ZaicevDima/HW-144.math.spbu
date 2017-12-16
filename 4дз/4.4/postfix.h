8ca92ed14c3d4ee50e763e132adee1901ff2a6a6
ZaicevDmitriy <zaicevdmitiy@gmail.com>
2017-12-16 21:01:38 +0300

    moved project(again)

--- 4дз/4.4/postfix.h
+++
@@ -1,8 +0,0 @@
-#pragma once;
-#include "stack.h"
-
-bool isDigit(char symbol);
-bool isSpace(char symbol);
-int numeral(char symbol);
-int action(char symbol, int number1, int number2);
-void printResult(char* symbol);
