ed0602d8ac3d0857013e629c2579889071aec88e
ZaicevDmitriy <zaicevdmitiy@gmail.com>
2017-12-17 20:24:32 +0300

    deleted 5.3 from root

--- 5дз/5.3/polinomial.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-int amountNumeral(int number);
-void lengthSpace(int *lengthSimbols,int *coefficients, int digit);
-void printDegrees(int digit, int *coefficients);
-void printSign(int number);
-void printSpaceBetweenCoefficient(int digit, int i);
-void printCoefficients(int digit, int *coefficients);
