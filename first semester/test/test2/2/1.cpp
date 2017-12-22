0945c4a94d6043654097308dcaadc0f8b5aaae18
ZaicevDmitriy <zaicevdmitiy@gmail.com>
2017-12-22 17:10:18 +0300

    new problem test2.2

---
+++ first semester/test/test2/2/1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int amountDigits(int number)
+{
+    int amount = 0;
+    while (number > 0)
+    {
+        amount++;
+        number /= 10;
+    }
+    return amount;
+}
+
+int reverse(int number)
+{
+    int newNumber = 0;
+    while (number > 0)
+    {
+        newNumber = newNumber * 10 + number % 10;
+        number /= 10;
+    }
+
+    return newNumber;
+}
+
+int main()
+{
+    ifstream fin;
+    ofstream fout;
+    fin.open("in.txt");
+    fout.open("out.txt");
+
+    if (!fin.good())
+    {
+        cout << "error, file not opened";
+        return 1;
+    }
+
+    while (!fin.eof())
+    {
+        int number;
+        fin >> number;
+        fout << reverse(number) << ' ';
+    }
+
+    fin.close();
+    fout.close();
+    return 0;
+}
