f675383678625a8cae8bd0585cedfccb1f8860ea
ZaicevDmitriy <zaicevdmitiy@gmail.com>
2017-12-23 13:46:07 +0300

    added print way to first city

--- first semester/hw9/9.1/main.cpp
+++ first semester/hw9/9.1/main.cpp
@@ -5,44 +5,65 @@ using namespace std;
 
 const int inf = 1000000;
 
-int* deixtr(int **edge, int n)
+void printWay(int* previous, int index)
 {
-    int *distance = new int[n];
-    fill(distance, distance + n, inf);
-    distance[0] = 0;
-    bool used[n];
-    fill (used, used + n, false);
-    for (int i = 0; i < n; i++)
+    cout << index + 1 << ' ';
+    while (previous[index] != -1)
     {
-        int minimum = inf;
-        int minNumber = -1;
-        for (int j = 0 ; j < n; j++)
-        {
-            if (!used[j] && (distance[j] < minimum))
-            {
-                minNumber = j;
-                minimum = distance[j];
-            }
-        }
-        used[minNumber] = true;
-        for (int j = 0; j < n; j++)
-            distance[j] = min(distance[j], distance[minNumber] + edge[minNumber][j]);
-        cout << minNumber + 1 << ' ';
+        cout << previous[index] + 1 << ' ';
+        index = previous[index];
     }
-    cout << "\n";
-    return distance;
 }
 
-void printDistation(int* distance, int n)
+void deixtra(int **adjacencyMatrix, int amountCities)
 {
-    for (int i = 0; i < n; i++)
+    int* previous = new int[amountCities];
+    int* distance = new int[amountCities];
+    fill(distance, distance + amountCities, inf);
+    distance[0] = 0;
+    bool* used = new bool[amountCities];
+    fill (used, used + amountCities, false);
+
+    int currentIndex = 0;
+    previous[currentIndex] = -1;
+    while (!used[currentIndex])
     {
-        cout << "distance of " << i + 1 << " to 1 = ";
-        if (distance[i] != inf)
-            cout << distance[i] << '\n';
-        else
-            cout << "inf\n";
-    }
+         used[currentIndex] = true;
+         cout << currentIndex + 1 << " ";
+         for (int i = 0; i < amountCities; i++)
+         {
+             if (!used[i] && adjacencyMatrix[currentIndex][i] && (distance[currentIndex] + adjacencyMatrix[currentIndex][i] < distance[i]))
+             {
+                 previous[i] = currentIndex;
+                 distance[i] = distance[currentIndex] + adjacencyMatrix[currentIndex][i];
+             }
+         }
+
+         int minDistation = inf;
+         for (int i = 0; i < amountCities; i++)
+         {
+             if (!used[i] && distance[i] < minDistation)
+             {
+                 minDistation = distance[i];
+                 currentIndex = i;
+             }
+         }
+     }
+
+     cout << "\nInformation about cities: \n";
+     for (int i = 0; i < amountCities; i++)
+     {
+        if (used[i])
+        {
+            cout <<"City: " << i + 1 << " distance = " << distance[i] <<" way to first city: ";
+            printWay(previous, i);
+            cout << "\n";
+        }
+     }
+
+    delete[] previous;
+    delete[] distance;
+    delete[] used;
 }
 
 int main()
@@ -54,27 +75,31 @@ int main()
         cout << "error, file not opened";
         return 1;
     }
-    int n = 0;
-    int m = 0;
-    fin >> n >> m;
-    int **a = new int*[n];
-    for (int i = 0; i < n; i++)
-        a[i] = new int[n];
+    int amountCities = 0;
+    int amountRoads = 0;
+    fin >> amountCities >> amountRoads;
+    int **adjacencyMatrix = new int*[amountCities];
+    for (int i = 0; i < amountCities; i++)
+        adjacencyMatrix[i] = new int[amountCities];
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            a[i][j] = inf;
+    for (int i = 0; i < amountCities; i++)
+        for (int j = 0; j < amountCities; j++)
+            adjacencyMatrix[i][j] = inf;
 
-    for (int k = 0; k < m; k++)
+    for (int k = 0; k < amountRoads; k++)
     {
         int i, j, len;
         fin >> i >> j >> len;
         i--;
         j--;
-        a[i][j] = len;
-        a[j][i] = len;
+        adjacencyMatrix[i][j] = len;
+        adjacencyMatrix[j][i] = len;
     }
     cout << "occupied cities: \n";
-    printDistation(deixtr(a, n), n);
+    deixtra(adjacencyMatrix, amountCities);
+
+    for (int i = 0; i < amountCities; i++)
+        delete[] adjacencyMatrix[i];
+    delete[] adjacencyMatrix;
     return 0;
 }
