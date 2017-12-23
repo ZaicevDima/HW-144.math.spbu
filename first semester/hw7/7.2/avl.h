051cb8c170ebed869562dcfb532a3221ee0ee213
ZaicevDima <33327548+ZaicevDima@users.noreply.github.com>
2017-12-23 13:56:05 +0300

    deleted space

--- first semester/hw7/7.2/avl.h
+++ first semester/hw7/7.2/avl.h
@@ -8,7 +8,6 @@ AVLTree* createAVLTree();
 void add(AVLTree* tree, int value);
 void remove(AVLTree* tree, int value);
 bool isBelong(AVLTree* tree, int value);
-
 bool isEmpty(AVLTree* tree);
 void deleteAVLTree(AVLTree *&tree);
 void printAVLTree(AVLTree* tree);
