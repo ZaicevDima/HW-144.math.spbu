981c2ade9c4022b426e46611911055e65713e629
ZaicevDima <zaicevdmitiy@gmail.com>
2017-12-27 22:07:19 +0300

    deleted comment, and file  pro.user

--- first semester/hw5/5.2/main.cpp
+++ first semester/hw5/5.2/main.cpp
@@ -4,41 +4,6 @@
 
 using namespace std;
 
-/*int *toBits(int number)
-{
-    int *bits = new int[32];
-    int mask = 1;
-    for (int i = 0; i < 32; i++)
-    {
-        if (number & mask)
-            bits[i] = 1;
-        else
-            bits[i] = 0;
-        mask = mask << 1;
-    }
-    return bits;
-}
-
-void printBits(int *number)
-{
-    for (int i = 31; i >= 0; i--)
-    {
-        cout << number[i];
-    }
-}
-
-int toNumber(int *bits)
-{
-    int degree = 1;
-    int result = 0;
-    for (int i = 0; i < 32; i++)
-    {
-        result += bits[i] * degree;
-        degree = degree << 1;
-    }
-    return result;
-}*/
-
 int main()
 {
     cout << "enter two numbers: \n";
