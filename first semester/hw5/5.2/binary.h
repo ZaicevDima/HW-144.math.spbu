87f29ce76d056dda89e388396beecac13e63f9d8
ZaicevDima <33327548+ZaicevDima@users.noreply.github.com>
2017-12-27 22:16:40 +0300

    Update binary.h

--- first semester/hw5/5.2/binary.h
+++ first semester/hw5/5.2/binary.h
@@ -6,6 +6,4 @@ const int maxLength = sizeof(int) * 8;
 void printBinaryTranslation(int* bytes);
 int* binaryTranslation(int number);
 int* sum(int* number1,int* number2);
-void toDirectCode(int *bytes);
-void printToDirectCode(int* bytes);
 int inDecimalSystem(int* bits);
