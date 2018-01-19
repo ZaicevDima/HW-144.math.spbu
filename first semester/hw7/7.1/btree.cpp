70e02c6b0be12a9feb8290819d9335e99a5b1297
ZaicevDima <zaicevdmitiy@gmail.com>
2018-01-19 14:38:49 +0300

    change deleteNode

--- first semester/hw7/7.1/btree.cpp
+++ first semester/hw7/7.1/btree.cpp
@@ -55,62 +55,56 @@ void addToHashTable(BinaryTree* tree, int value)
     addToHashTable(tree->root, value);
 }
 
-void removeNode(Node *&node)
+Node *findMax(Node *node)
 {
-    if (node == nullptr)
-        return;
-
-    if ((node->left == nullptr) && (node->right == nullptr))
-    {
-        delete node;
-        node = nullptr;
-    }
-
-    else if ((node->left != nullptr) && (node->right == nullptr))
-    {
-        Node* tempNode = node;
-        node = node->left;
-        delete tempNode;
-    }
-
-    else if ((node->left == nullptr) && (node->right != nullptr))
-    {
-        Node* tempNode = node;
+    while (node->right != nullptr)
         node = node->right;
-        delete tempNode;
-    }
-
-    else if ((node->left != nullptr) && (node->right != nullptr))
-    {
-        Node* tempNode = node->left;
-        while ((tempNode)->right != nullptr)
-            tempNode = (tempNode)->right;
-        node->value = (tempNode)->value;
-        removeNode(tempNode);
-        return;
-    }
+    return node;
 }
 
-void remove(Node *&node, int value)
+void removeNode(Node *&node, int value)
 {
     if (node == nullptr)
         return;
 
     if (node->value == value)
     {
-        removeNode(node);
+        if ((node->left == nullptr) && (node->right == nullptr))
+        {
+            delete node;
+            node = nullptr;
+        }
+        else if (node->left == nullptr)
+        {
+            Node *temp = node->right;
+            delete node;
+            node = temp;
+        }
+        else if (node->right == nullptr)
+        {
+            Node *temp = node->left;
+            delete node;
+            node = temp;
+        }
+        else
+        {
+            Node *maxNode = findMax(node->left);
+            int maxValue = maxNode->value;
+            removeNode(node, maxValue);
+            node->value = maxValue;
+        }
         return;
     }
 
-    if (node->value < value)
-        remove(node->right, value);
+    if (value < node->value)
+        return removeNode(node->left, value);
     else
-        remove(node->left, value);
+        return removeNode(node->right, value);
 }
 
 void remove(BinaryTree* tree, int value)
 {
-    remove(tree->root, value);
+    return removeNode(tree->root, value);
 }
 
 bool isBelong(Node* node, int value)
