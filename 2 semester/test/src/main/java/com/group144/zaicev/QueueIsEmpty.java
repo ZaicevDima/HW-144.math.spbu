2680bd8ae4ea426f47b64779ac358e1f65cece5b
ZaicevDima <zaicevdmitiy@gmail.com>
2018-03-20 16:08:40 +0300

    rename

--- 2 semester/test/src/main/java/com/group144/zaicev/QueueIsEmpty.java
+++ 2 semester/test/src/main/java/com/group144/zaicev/QueueIsEmpty.java
@@ -2,7 +2,7 @@ package com.group144.zaicev;
 
 /** Throw if queue is empty */
 class QueueIsEmpty extends Throwable {
-    QueueIsEmpty(String messege) {
-        super(messege);
+    QueueIsEmpty(String message) {
+        super(message);
     }
 }
