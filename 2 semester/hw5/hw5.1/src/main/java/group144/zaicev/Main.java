dde9867a9bb21f6b150549469d0fbe518ba5535a
ZaicevDima <33327548+ZaicevDima@users.noreply.github.com>
2018-03-30 23:07:00 +0300

    Update Main.java

--- 2 semester/hw5/hw5.1/src/main/java/group144/zaicev/Main.java
+++ 2 semester/hw5/hw5.1/src/main/java/group144/zaicev/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("slider.fxml"));
         primaryStage.setTitle("Slider");
         primaryStage.setScene(new Scene(root, 300, 300));
