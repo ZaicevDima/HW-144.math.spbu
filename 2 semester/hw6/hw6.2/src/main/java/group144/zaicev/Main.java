24180ebe05895dbba872fd51ecb0f8706310dcee
ZaicevDima <zaicevdmitiy@gmail.com>
2018-04-25 20:40:36 +0300

    fixed zoom

--- 2 semester/hw6/hw6.2/src/main/java/group144/zaicev/Main.java
+++ 2 semester/hw6/hw6.2/src/main/java/group144/zaicev/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ticTacToe.fxml"));
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
